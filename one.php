1eceae5112f8c54eac48619c6fe1010bd37de97e
sahininayanthara <sahininayanthara95@gmail.com>
2019-03-23 08:52:17 +0530

    first commit

---
+++ one.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+<?php
+
+$x=10;
+$y=10;
+
+function cal($x,$y){
+
+$sum=$x+$y;
+echo "The answer is: ".$sum;
+
+}
+cal($x,$y);
+
+echo "<br>";
+$x=50;
+$y=20;
+cal($x,$y);
+
+?>
+
+</body>
+</html>
